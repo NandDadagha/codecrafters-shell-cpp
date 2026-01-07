cd97e89e03441c0b7170511a88186e0d1e18ad60
NandDadagha <npdadagha@gmail.com>
2026-01-07 11:29:19 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,16 @@ int main()
     std::cout << "$ ";
     string command;
     getline(cin, command);
-    if(command == "exit") exit(0);
+    if (command == "exit")
+      exit(0);
+    if (command.rfind("echo", 0) == 0)
+    {
+      string remaining = command.substr(4);
+      if (!remaining.empty() && remaining[0] == ' ')
+        remaining.erase(0, remaining.find_first_not_of(' '));
+      cout << remaining << endl;
+      continue;
+    }
     cout << command << ": command not found" << endl;
   }
 }
