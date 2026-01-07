41c5168d6755b4373fe05b18b4de04db986cc89d
NandDadagha <npdadagha@gmail.com>
2026-01-07 11:42:36 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -14,14 +14,33 @@ int main()
     std::cout << "$ ";
     string command;
     getline(cin, command);
+
+    // exit
     if (command == "exit")
       exit(0);
+
+    // echo
     if (command.rfind("echo", 0) == 0)
     {
       string remaining = command.substr(4);
       if (!remaining.empty() && remaining[0] == ' ')
         remaining.erase(0, remaining.find_first_not_of(' '));
       cout << remaining << endl;
+      continue;
+    }
+
+    // type
+    if (command.rfind("type", 0) == 0)
+    {
+      string remaining = command.substr(4);
+      if (!remaining.empty() && remaining[0] == ' ')
+        remaining.erase(0, remaining.find_first_not_of(' '));
+      if(remaining == "echo" || remaining == "exit" || remaining == "type") {
+        cout << remaining << " is a shell builtin\n";
+      }
+      else {
+        cout << remaining << ": not found\n";
+      }
       continue;
     }
     cout << command << ": command not found" << endl;
