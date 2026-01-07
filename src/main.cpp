553193b9ae5fb4ea0ac6422c6fc335f0f0a799ef
NandDadagha <npdadagha@gmail.com>
2026-01-07 13:56:56 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
+#include <cstdlib>    // getenv
+#include <sstream>    // stringstream
+#include <unistd.h>   // access
+#include <sys/stat.h> // stat
 using namespace std;
+
+bool isExecutable(const string &path)
+{
+  struct stat sb;
+  return stat(path.c_str(), &sb) == 0 &&
+         S_ISREG(sb.st_mode) &&
+         access(path.c_str(), X_OK) == 0;
+}
 
 int main()
 {
@@ -35,13 +46,40 @@ int main()
       string remaining = command.substr(4);
       if (!remaining.empty() && remaining[0] == ' ')
         remaining.erase(0, remaining.find_first_not_of(' '));
-      if(remaining == "echo" || remaining == "exit" || remaining == "type") {
+
+      if (remaining == "echo" || remaining == "exit" || remaining == "type")
+      {
         cout << remaining << " is a shell builtin\n";
+        continue;
       }
-      else {
+      const char *pathEnv = getenv("PATH");
+      if (!pathEnv)
+      {
         cout << remaining << ": not found\n";
+        continue;
       }
-      continue;
+      stringstream ss(pathEnv);
+      string dir;
+      bool flag = false;
+      while (getline(ss, dir, ':'))
+      {
+        string fullPath = dir + "/" + remaining;
+
+        // does file exists?
+        if (access(fullPath.c_str(), F_OK) == 0)
+        {
+          // is it executable?
+          if (isExecutable(fullPath))
+          {
+            cout << remaining << " is " << fullPath << "\n";
+            flag = true;
+            break;
+          }
+        }
+      }
+      if(!flag)
+        cout << remaining << ": not found\n";
+    continue;
     }
     cout << command << ": command not found" << endl;
   }
