d43f990569360d594656218393c2b58713f2e87a
NandDadagha <npdadagha@gmail.com>
2026-01-07 11:10:38 +0000

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
-  string command;
-  getline(cin, command);
-  cout << command << ": command not found";
+  while (1)
+  {
+    std::cout << "$ ";
+    string command;
+    getline(cin, command);
+    cout << command << ": command not found" << endl;
+  }
 }
